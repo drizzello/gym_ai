064bdfb73ded3e0efa710b79e4cd0b4249a009e0
Davide Rizzello <84180408+drizzello@users.noreply.github.com>
2024-01-16 00:02:01 +0100

    Update prove.py

--- prove.py
+++ prove.py
@@ -1,16 +1,18 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.output_parsers import StrOutputParser
-output_parser = StrOutputParser()
+import streamlit as st
+from langchain.llms import OpenAI
 
-from openai import OpenAI
-client = OpenAI()
+st.title('🦜🔗 Quickstart App')
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
-print(completion.choices[0].message)
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
